9d7e61296a16c7cdf30abce0a9dce95694f72645
Isa <isa.nafieiev@gmail.com>
2023-06-06 15:45:32 -0700

    asn2

--- asn2.js
+++ asn2.js
@@ -1,28 +1,34 @@
-const studentData = [
-    {
-      name: 'Student 1',
-      weight: '150 lbs',
-      height: '65 inches',
-      hairColor: 'blonde',
-      gpa: '3.5'
-    },
-    {
-      name: 'Student 2',
-      weight: '130 lbs',
-      height: '63 inches',
-      hairColor: 'brown',
-      gpa: '3.2'
-    },
-    {
-      name: 'Student 3',
-      weight: '170 lbs',
-      height: '68 inches',
-      hairColor: 'black',
-      gpa: '3.8'
-    }
-  ];
+const studentData = [];
+
+function addStudent() {
+  const nameInput = document.querySelector('.name-input');
+  const weightInput = document.querySelector('.weight-input');
+  const heightInput = document.querySelector('.height-input');
+  const hairColorInput = document.querySelector('.hair-color-input');
+  const gpaInput = document.querySelector('.gpa-input');
   
+  const student = {
+    name: nameInput.value,
+    weight: weightInput.value,
+    height: heightInput.value,
+    hairColor: hairColorInput.value,
+    gpa: gpaInput.value
+  };
+  
+  studentData.push(student);
+  
+  nameInput.value = '';
+  weightInput.value = '';
+  heightInput.value = '';
+  hairColorInput.value = '';
+  gpaInput.value = '';
+  
+  displayStudentBoxes();
+}
+
+function displayStudentBoxes() {
   const studentBoxesContainer = document.getElementById('student-boxes');
+  studentBoxesContainer.innerHTML = '';
   
   studentData.forEach(student => {
     const box = document.createElement('div');
@@ -43,4 +49,4 @@ const studentData = [
     
     studentBoxesContainer.appendChild(box);
   });
-  
+}
