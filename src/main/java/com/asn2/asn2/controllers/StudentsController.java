77b336942f3fb51f39e9ebae1f40940e6dbede6a
Isa <isa.nafieiev@gmail.com>
2023-06-08 12:31:38 -0700

    asn2

--- src/main/java/com/asn2/asn2/controllers/StudentsController.java
+++ src/main/java/com/asn2/asn2/controllers/StudentsController.java
@@ -4,19 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentsController {
     @GetMapping("/students/view")
-    public String getAllStudents(){
+    public String getAllStudents(Model){
         System.out.println("Getting all students");
-        // TODO: get all users from database
+        // TODO: get all students from database
         List<Students> students = new ArrayList<>();
         students.add(new Students(name:"isa", height:"180", weight:"70", hairColor:"brown", GPA:"3.2"));
         students.add(new Students(name:"john", height:"135", weight:"60", hairColor:"blue", GPA:"4.1"));
         students.add(new Students(name:"jack", height:"189", weight:"78", hairColor:"black", GPA:"4.0"));
         students.add(new Students(name:"knan", height:"176", weight:"77", hairColor:"red", GPA:"2.8"));
+        // end of database call
+        model.addAttribute(attributeName:"st", students);
         return "students/showAll";
     }
 }
