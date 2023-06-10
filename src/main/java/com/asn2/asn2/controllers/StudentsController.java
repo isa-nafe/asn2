ac0b0d30851bdb1ef67c15b8ec831c1b9284c3f5
Isa <isa.nafieiev@gmail.com>
2023-06-10 14:50:34 -0700

    asn2

--- src/main/java/com/asn2/asn2/controllers/StudentsController.java
+++ src/main/java/com/asn2/asn2/controllers/StudentsController.java
@@ -1,40 +1,34 @@
 package com.asn2.asn2.controllers;
-import com.asn2.asn2.models.StudentRepository;
-import com.asn2.asn2.models.Students;
 
-import java.util.ArrayList;
+import com.asn2.asn2.models.Students;
+import com.asn2.asn2.models.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-@Controller
+@RestController
+@RequestMapping("/students")
 public class StudentsController {
-    @GetMapping("/students/view")
-    public String getAllStudents(Model model) {
-        System.out.println("Getting all students");
-        // TODO: get all students from the database
-        List<Students> students = new ArrayList<>();
-        students.add(new Students("isa", 180, 70, "brown", 3.2));
-        students.add(new Students("john", 135, 60, "blue", 4.1));
-        students.add(new Students("jack", 189, 78, "black", 4.0));
-        students.add(new Students("knan", 176.8, 77.6, "red", 2.8));
-        // end of database call
-        model.addAttribute("st", students);
-        return "students/showAll";
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentsController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
 
-    @GetMapping("/dtudents/view/{uid}")
-    public String getStudent(Model model, @PathVariable String uid){
-        System.out.println("Get Student:" + uid);
+    @GetMapping
+    public List<Students> getAllStudents() {
+        return studentRepository.findAll();
+    }
 
-        int id = Integer.parseInt(uid);
-        Students st = StudentRepository.findByName(uid).get();
-        StudentRepository.delete(st);
+    @PostMapping
+    public Students addStudent(@RequestBody Students student) {
+        return studentRepository.save(student);
+    }
 
-        model.addAttribute("student");
-        return "showStudent";
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentRepository.deleteById(id);
     }
 }
--- src/main/java/com/asn2/asn2/models/StudentRepository.java
+++ src/main/java/com/asn2/asn2/models/StudentRepository.java
@@ -3,5 +3,4 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Students, Long>{
-    List<Students> findByName(String name);
 }
